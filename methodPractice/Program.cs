9d19748a7f750a909da893612348ccf35399212d
bradberger01 <bradberger@hotmail.com>
2017-08-31 20:48:32 -0400

    method practices

--- methodPractice/Program.cs
+++ methodPractice/Program.cs
@@ -10,6 +10,36 @@ namespace methodPractice
     {
         static void Main(string[] args)
         {
+            int number1 = 25;
+            int number2 = 50;
+            int total = Add(number1, number2);
+            //Console.WriteLine(total);
+
+            //Console.WriteLine(Multiply(number1, number2));
+            Greeting();
+            string name = "Will Robinson";
+            RobotWArning(name);
         }
-    }
+        public static int Add(int firstNumber, int secondNumber)
+        {
+        int sum = firstNumber + secondNumber;
+
+        return sum;
+        }
+        public static int Multiply(int firstNumber, int secondNumber)
+        {
+            int product = firstNumber * secondNumber;
+
+            return product;
+        }
+        public static void Greeting()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        public static void RobotWArning(string name)
+        {
+            Console.WriteLine("Danger, " + name);
+        }
+}
 }
